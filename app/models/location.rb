cee375f9e35bc294f2a80d30f37f24dd5755ac19
LandonWest <landonwest5@gmail.com>
2014-07-11 10:56:49 -0600

    add location model

---
+++ app/models/location.rb
@@ -0,0 +1,8 @@
+class Location < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode
+
+  def address
+    "#{street_address} #{street_address2}, #{city}, #{state} #{zip}, US"
+  end
+end
