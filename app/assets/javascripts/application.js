03a9bf7ad4b8ed300f08f76c1ba8f02cfe2a4fc2
LandonWest <landonwest5@gmail.com>
2014-07-11 11:57:43 -0600

    add map and location marker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,29 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require gmaps
 //= require_tree .
+
+$(function() {
+  loadMap();
+});
+
+var loadMap = function() {
+  $map = $('#map');
+  if ($map.length) {
+    var gmap = new GMaps({
+      div: '#map',
+      lat: $map.data('lat'),
+      lng: $map.data('lon')
+    });
+
+    gmap.addMarker({
+      lat: $map.data('lat'),
+      lng: $map.data('lon'),
+      title: 'Home',
+      infoWindow: {
+        content: '<p>' + $map.data('address') + '</p>'
+      }
+    });
+  }
+};
